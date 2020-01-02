a961b8b7981c92578d6028014712e13cd08825f3
Richard Jones <richard@cottagelabs.com>
2020-01-02 11:03:02 +0000

    add proper swordbagit zip deposit test

---
+++ sword3client/lib/paths.py
@@ -0,0 +1,21 @@
+import os
+import hashlib
+
+def rel2abs(file, *args):
+    file = os.path.realpath(file)
+    if os.path.isfile(file):
+        file = os.path.dirname(file)
+    return os.path.abspath(os.path.join(file, *args))
+
+def list_subdirs(path):
+    return [x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))]
+
+def sha256(path, buffer_size=65536):
+    digest = hashlib.sha256()
+    with open(path, "rb") as f:
+        while True:
+            data = f.read(buffer_size)
+            if not data:
+                break
+            digest.update(data)
+    return digest
--- sword3client/test/unit/test_binary_deposit.py
+++ sword3client/test/unit/test_binary_deposit.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from sword3client.client import SWORD3Client
 from sword3client.test.mocks.connection import MockHttpLayer
+from sword3client.lib import paths
 from sword3common.test.fixtures.status import StatusFixtureFactory
 from sword3common.lib.seamless import SeamlessException
 from sword3common import constants
@@ -33,3 +34,23 @@ class TestBinaryDeposit(TestCase):
         except SeamlessException as e:
             print(e.message)
 
+    def test_02_create_object_with_package(self):
+
+        SD_URL = "http://example.com/service-document"
+        BODY = json.dumps(StatusFixtureFactory.status_document())
+        HEADERS = {"Location" : "http://example.com/location"}
+
+        client = SWORD3Client(http=MockHttpLayer(201, BODY, HEADERS))
+
+        bag = paths.rel2abs(__file__, "..", "resources", "SWORDBagIt.zip")
+        d = paths.sha256(bag)
+        digest = {
+            constants.DIGEST_SHA_256: base64.b64encode(d.digest())
+        }
+        with open(bag, "rb") as stream:
+            try:
+                dr = client.create_object_with_package(SD_URL, stream, "test.zip", digest,
+                                                       content_type="application/zip",
+                                                       packaging=constants.PACKAGE_SWORDBAGIT)
+            except SeamlessException as e:
+                print(e.message)
