59f728c4d5a0c3c171d30f897db20957df1d626d
Alex Dutton <alex@cottagelabs.com>
2020-07-17 17:55:04 +0100

    Bump version for PyPI release, and include README as long_description

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="sword3client",
-    version="0.0.1",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
         'sword3common',
     ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url="http://cottagelabs.com/",
     author="Cottage Labs",
     author_email="richard@cottagelabs.com",
